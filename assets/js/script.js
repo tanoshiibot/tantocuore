08cf9eff3d254d1eee2de6d3a7677436fe28d740
tanoshiibot <fulljelly@outlook.fr>
2020-11-17 14:18:05 +0100

    form post pour envoyer les paramètres

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let toggle = document.getElementById("allowAttacks").checked;
+
+document.getElementById("allowAttacks").addEventListener("click", () => {
+    toggle = document.getElementById("allowAttacks").checked;
+    console.log(toggle);
+    document.getElementById("attackRequire").setAttribute("class", toggle ? "d-flex justify-content-between col-2 offset-10" : "d-none ")
+})
--- index.php
+++ index.php
@@ -13,33 +13,41 @@ $games = json_decode($json, true);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Tanto Cuore Randomizer</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
-    <?php
-        print_r($games);
-    ?>
-    <button id="run">Run</button>
-    <section id="card_container">
 
-    </section>
-    <form>
-        <input type="checkbox" id="actions">
-        <label for="actions"> Require +2 Actions</label>
-        <input type="checkbox" id="drawer">
-        <label for="drawer"> Require Drawer</label>
-        <input type="checkbox" id="buy">
-        <label for="buy"> Require Buy</label>
-        <input type="checkbox" id="attack">
-        <label for="attack"> Allow Attacks</label>
-        <input type="checkbox" id="attack_r">
-        <label for="attack_r"> Require Attack</label>
+    <form action="results.php" method="post" class="d-flex flex-column justify-content-center container-xl">
+        <div class="d-flex justify-content-between col-2 offset-10">
+            <label for="actions"> Require +2 Actions</label>
+            <input type="checkbox" id="actions" name="requireAction">
+        </div>
+        <div class="d-flex justify-content-between col-2 offset-10">
+            <label for="drawer"> Require Drawer</label>
+            <input type="checkbox" id="drawer" name="requireDrawer">
+        </div>
+        <div class="d-flex justify-content-between col-2 offset-10">
+            <label for="buy"> Require Buy</label>
+            <input type="checkbox" id="buy" name="requireBuy">
+        </div>
+        <div class="d-flex justify-content-between col-2 offset-10">
+            <label for="allowAttacks"> Allow Attacks</label>
+            <input type="checkbox" id="allowAttacks" name="allowAttacks">
+        </div>
+        <div id="attackRequire">
+            <label for="requireAttack"> Require Attack</label>
+            <input type="checkbox" id="requireAttack" name="requireAttack">
+        </div>
+
+        <input type="submit" value="submit" class="col-2 offset-10">
     </form>
 
     <table>
         <?php
         echo "<p>Liste des cartes :</p>";
+
         for ($i = 0; $i < (count($games["Tanto Cuore"]) / 6); $i++)
         {
             echo "<tr>";
@@ -52,5 +60,6 @@ $games = json_decode($json, true);
 
         ?>
     </table>
+    <script src="./assets/js/script.js"></script>
 </body>
 </html>
