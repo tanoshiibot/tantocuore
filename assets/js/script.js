c925dca1bb7ff6b79aacfd9ea6a1fcbc625b3dd0
tanoshiibot <fulljelly@outlook.fr>
2020-11-18 09:35:01 +0100

    correction du form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,10 @@
 let allowToggle = document.getElementById("allowAttacks").checked;
-let requireToggle = document.getElementById("requireAttack").checked;
 
 allowToggle ? document.getElementById("requireAttack").removeAttribute("disabled") : document.getElementById("requireAttack").setAttributeNode(document.createAttribute("disabled"))
-!requireToggle ? document.getElementById("allowAttacks").removeAttribute("disabled") : document.getElementById("allowAttacks").setAttributeNode(document.createAttribute("disabled"))
 
 
 document.getElementById("allowAttacks").addEventListener("click", () => {
     allowToggle = document.getElementById("allowAttacks").checked;
     allowToggle ? document.getElementById("requireAttack").removeAttribute("disabled") : document.getElementById("requireAttack").setAttributeNode(document.createAttribute("disabled"))
-
-})
-
-document.getElementById("requireAttack").addEventListener("click", () => {
-    requireToggle = document.getElementById("requireAttack").checked;
-    !requireToggle ? document.getElementById("allowAttacks").removeAttribute("disabled") : document.getElementById("allowAttacks").setAttributeNode(document.createAttribute("disabled"))
 
 })
